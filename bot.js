a38392e1e352ac9d4cdf33b956ac06e7cfcfca1e
Felix <f.brandmueller@gmail.com>
2016-11-29 23:32:40 +0100

    get_Information_text_#10

--- bot.js
+++ bot.js
@@ -28,6 +28,15 @@ const firstEntityValue = (entities, entity) => {
   return typeof val === 'object' ? val.value : val;
 };
 
+//formats the given message
+function formatmsg(msg) {
+    msg = msg.substr(0, 320);
+    if (msg.lastIndexOf(".") === -1) {
+        return msg;
+    }
+    return msg.substr(0, msg.lastIndexOf(".") + 1);
+}
+
 const actions = {
   send(request, response) {
     const {sessionId, context, entities} = request;
@@ -37,6 +46,7 @@ const actions = {
       return resolve();
     });
   },
+    //example method
   getForecast({context, entities}) {
     return new Promise(function(resolve, reject) {
       var location = firstEntityValue(entities, 'location')
@@ -49,8 +59,41 @@ const actions = {
       }
       return resolve(context);
     });
-  },
-};
+},
+
+    // getInformation bot executes
+    getInformation({context,entities}) {
+        return new Promise(function(resolve,reject){
+
+            var search_query = firstEntityValue(entities,"search_query");
+            if (search_query){
+                var queryUrl = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=1&prop=extracts&exintro&explaintext&exsentences=5&exlimit=max&gsrsearch=" + search_query;
+
+                var request = require("request");
+                request(queryUrl, function (error, response, body) {
+                    //statusCode 200 = "OK"
+                    if (!error && response.statusCode === 200) {
+                        try {
+                            body = JSON.parse(body);
+                            var pages = body.query.pages;
+                            var pageId = Object.keys(pages)[0];
+                            var text = pages[pageId].extract;
+                            context.information = formatmsg(text);
+                        }
+                        catch (err) {
+                            context.information = "Sorry I didn't get that, can you modify your question?";
+                        }
+                    } else {
+                        context.information = "Connection Error: "+ response.statusCode;
+                    }
+                    return resolve(context);
+                });
+            } else {
+              context.information = "search_query not found";
+            }
+        });
+    }
+}
 
 const client = new Wit({accessToken, actions});
 interactive(client);
