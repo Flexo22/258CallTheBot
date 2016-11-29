25ccfbed96ab99a89a81d8c4e68c76a730d21677
bgeVam <bernold.georg@gmail.com>
2016-11-29 18:35:19 +0100

    test node version

--- bot.js
+++ bot.js
@@ -1,93 +1,56 @@
 'use strict';
 
-// Weather Example
-// See https://wit.ai/sungkim/weather/stories and https://wit.ai/docs/quickstart
-const Wit = require('node-wit').Wit;
-const FB = require('./facebook.js');
-const Config = require('./const.js');
+let Wit = null;
+let interactive = null;
+try {
+  // if running from repo
+  Wit = require('../').Wit;
+  interactive = require('../').interactive;
+} catch (e) {
+  Wit = require('node-wit').Wit;
+  interactive = require('node-wit').interactive;
+}
+
+const accessToken = "YTTNUWDR3IXFVAOPWPCRHLKZWQHT2RI5";
+
+// Quickstart example
+// See https://wit.ai/ar7hur/quickstart
 
 const firstEntityValue = (entities, entity) => {
   const val = entities && entities[entity] &&
     Array.isArray(entities[entity]) &&
     entities[entity].length > 0 &&
-    entities[entity][0].value;
+    entities[entity][0].value
+  ;
   if (!val) {
     return null;
   }
   return typeof val === 'object' ? val.value : val;
 };
 
-// Bot actions
 const actions = {
-  say(sessionId, context, message, cb) {
-    console.log(message);
-
-    // Bot testing mode, run cb() and return
-    if (require.main === module) {
-      cb();
-      return;
-    }
-
-    // Our bot has something to say!
-    // Let's retrieve the Facebook user whose session belongs to from context
-    // TODO: need to get Facebook user name
-    const recipientId = context._fbid_;
-    if (recipientId) {
-      // Yay, we found our recipient!
-      // Let's forward our bot response to her.
-      FB.fbMessage(recipientId, message, (err, data) => {
-        if (err) {
-          console.log(
-            'Oops! An error occurred while forwarding the response to',
-            recipientId,
-            ':',
-            err
-          );
-        }
-
-        // Let's give the wheel back to our bot
-        cb();
-      });
-    } else {
-      console.log('Oops! Couldn\'t find user in context:', context);
-      // Giving the wheel back to our bot
-      cb();
-    }
+  send(request, response) {
+    const {sessionId, context, entities} = request;
+    const {text, quickreplies} = response;
+    return new Promise(function(resolve, reject) {
+      console.log('sending...', JSON.stringify(response));
+      return resolve();
+    });
   },
-  merge(sessionId, context, entities, message, cb) {
-    // Retrieve the location entity and store it into a context field
-    const loc = firstEntityValue(entities, 'location');
-    if (loc) {
-      context.loc = loc; // store it in context
-    }
-
-    cb(context);
-  },
-
-  error(sessionId, context, error) {
-    console.log(error.message);
-  },
-
-  // getInformation bot executes
-  ['getInformation'](sessionId, context, cb) {
-    // Here should go the api call, e.g.:
-    // context.forecast = apiCall(context.loc)
-    context.information = 'awesome!' ;
-    cb(context);
+  getForecast({context, entities}) {
+    return new Promise(function(resolve, reject) {
+      var location = firstEntityValue(entities, 'location')
+      if (location) {
+        context.forecast = 'sunny in ' + location; // we should call a weather API here
+        delete context.missingLocation;
+      } else {
+        context.missingLocation = true;
+        delete context.forecast;
+      }
+      return resolve(context);
+    });
   },
 };
 
-
-const getWit = () => {
-  return new Wit(Config.WIT_TOKEN, actions);
-};
-
-exports.getWit = getWit;
-
-// bot testing mode
-// http://stackoverflow.com/questions/6398196
-if (require.main === module) {
-  console.log("Bot testing mode.");
-  const client = getWit();
-  client.interactive();
-}
+const client = new Wit({accessToken, actions});
+interactive(client);
