bbfd6c900ef6767248051178a8fe4bb7e1036dbc
Felix <f.brandmueller@gmail.com>
2016-12-17 16:56:23 +0100

    add_MessageForwarding_#27

--- bot.js
+++ bot.js
@@ -140,7 +140,6 @@ const actions = {
             var text = "This chat needs a therapist: " + threadID.toString();
             // meanwhile hardcoded Jeany Doe
             var userID = "100014478432070";
-
             fbMessage(userID, text);
             //context.information = "A therapist is informed";
             return resolve(context);
@@ -218,13 +217,13 @@ app.get("/webhook", (req, res) => {
 app.post("/webhook", (req, res) => {
   // Parsing the Messenger API response
   const messaging = FB.getFirstMessagingEntry(req.body);
+  const threadID = request("threads").get(id);
   if (messaging && messaging.message) {
 
     // Yay! We got a new message!
 
     // We retrieve the Facebook user ID of the sender
     const sender = messaging.sender.id;
-    var threadID = Threads.get(id);
 
     // We retrieve the user's current session, or create one if it doesn't exist
     // This is needed for our bot to figure out the conversation history
