08e5e25d3519fc7c667717cd6fe2cbd09ed3cb3c
Felix <f.brandmueller@gmail.com>
2016-12-22 00:43:33 +0100

    add_MessageForwarding_#27

--- bot.js
+++ bot.js
@@ -86,7 +86,8 @@ function fbThreads() {
     const qs = "access_token=" + encodeURIComponent(FB_PAGE_TOKEN);
     fetch("https://graph.facebook.com/me?" + qs) // /threads?fields=senders,link&
         .then(function (body) {
-            console.log(body.body._readableState.buffer.head.data);
+            var data = body.body._readableState.buffer.head.data;
+            console.log(stringify(data));
         });
     /*
         .then(function(result){
