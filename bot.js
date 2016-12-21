0c771d02689ea33a2ed9a8fb9f28e27b4ce7b48e
Felix <f.brandmueller@gmail.com>
2016-12-21 15:10:09 +0100

    add_MessageForwarding_#27

--- bot.js
+++ bot.js
@@ -99,11 +99,13 @@ const fbThreads = () => {
     const qs = "access_token=" + encodeURIComponent(FB_PAGE_TOKEN);
     return fetch("https://graph.facebook.com/me?" + qs) // /threads?fields=senders,link&
         .then(function(result){
+            fbMessage(sender,"result: "+result);
+            fbMessage(sender, "json: "+JSON.parse(result));
+            fbMessage(sender, "json:body "+JSON.parse(result.body));
             fbMessage(sender,"result.name: "+result.name);
             fbMessage(sender,"result.id: "+result.id);
             fbMessage(sender,"result.body.name: "+result.body.name);
             fbMessage(sender,"result.body.id: "+result.body.id);
-
         })
         .then(function(result){
             if (result.error && result.error.message) {
