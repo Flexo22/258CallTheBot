489a026342cee1bffce302a5e3d14fa94ac5a6d2
Felix <f.brandmueller@gmail.com>
2017-03-22 00:02:15 +1100

    add_MessageForwarding_#27

--- facebook.js
+++ facebook.js
@@ -52,7 +52,7 @@ function getData (apiPath,callback) {
             buffer += chunk;
         });
         result.on('end', function () {
-            return callback(new Promise.resolve(buffer));
+            return callback(Promise.resolve(buffer));
         });
     });
 
